c15cc195418b7f92d894b07bf41f35601b3b95fd
Aimee Gamble-Milner <aimee.gamble@gmail.com>
2018-10-20 22:43:43 +0100

    Updated dependencies and removed ts-ignores

--- src/demo/assets/demo.tsx
+++ src/demo/assets/demo.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//@ts-ignore
 import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/prism-light';
-//@ts-ignore
 import jsx from 'react-syntax-highlighter/languages/prism/jsx';
 import { okaidia } from 'react-syntax-highlighter/styles/prism';
 
