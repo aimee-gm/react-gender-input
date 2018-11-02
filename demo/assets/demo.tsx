03a9f252b8488aa8e60621fad7b2218b62ea7bcb
Aimee Gamble-Milner <aimee.gamble@gmail.com>
2018-11-02 12:59:04 +0000

    Add demo app component test

--- demo/assets/demo.tsx
+++ demo/assets/demo.tsx
@@ -1,68 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import autobind from 'autobind-decorator';
-
+import { DemoApp } from './components/demo-app';
 import './demo.scss';
-
-import { CodeBlock } from './components/code-block';
-import { GenderInputDemo } from './components/gender-input-demo';
-import { PropToggle } from './components/prop-toggle';
-
-interface DemoState {
-	[key: string]: string | boolean | null | undefined;
-	gender: string | null | undefined;
-	preferNotToSay: boolean;
-	required: boolean;
-}
-
-class DemoApp extends React.Component<{}, DemoState> {
-	constructor(props: {}) {
-		super(props);
-		this.state = {
-			gender: undefined,
-			required: false,
-			preferNotToSay: true,
-		};
-	}
-
-	private get demoState() {
-		return { gender: this.state.gender };
-	}
-
-	private get genderProps() {
-		return {
-			required: this.state.required,
-			preferNotToSay: this.state.preferNotToSay,
-			name: 'gender-input',
-			onUpdate: (gender: string | null) => {
-				this.setState({
-					gender,
-				});
-			},
-		};
-	}
-
-	@autobind
-	private setParam(name: string, val: boolean) {
-		this.setState({ [name]: val });
-	}
-
-	render() {
-		return (
-			<div>
-				<h1>react-gender-input</h1>
-				<h2>Demo</h2>
-				<GenderInputDemo genderProps={this.genderProps} />
-				<h2>Markup</h2>
-				<CodeBlock language="jsx">{GenderInputDemo.markup(this.genderProps)}</CodeBlock>
-				<h2>Output</h2>
-				<CodeBlock language="json" code={this.demoState} />
-				<h2>Parameters</h2>
-				<PropToggle name="required" current={this.state.required} onClick={this.setParam} />
-				<PropToggle name="preferNotToSay" current={this.state.preferNotToSay} onClick={this.setParam} />
-			</div>
-		);
-	}
-}
 
 ReactDOM.render(<DemoApp />, document.getElementById('demo'));
