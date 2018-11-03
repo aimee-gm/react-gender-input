ed04f9f264a6698f018967848324e64bdb27b0f0
Aimee Gamble-Milner <aimee.gamble@gmail.com>
2018-11-03 14:03:54 +0000

    Simplify testing

--- test/.setup.ts
+++ test/.setup.ts
@@ -1,18 +1,4 @@
 import Enzyme from 'enzyme';
-import React from 'react';
-import Adapter16 from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 
-function configure() {
-	const matches = React.version.match(/^[0-9]+/);
-
-	if (matches && matches[0]) {
-		switch (matches[0]) {
-			case '16':
-				return Enzyme.configure({ adapter: new Adapter16() });
-		}
-	}
-
-	throw new Error(`Cannot determine React version from "${React.version}"`);
-}
-
-configure();
+Enzyme.configure({ adapter: new Adapter() });
